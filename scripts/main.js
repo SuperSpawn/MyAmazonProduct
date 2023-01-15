fc27af669fdf64913fbcddea07fce86ac1763770
SuperSpawn <thearktitan@gmail.com>
2023-01-15 08:27:59 +0200

    first

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const smallImages = document.querySelectorAll(".small-image");
+const bigImage = document.querySelector(".big-image");
+
+const onEnter = (event,index) => {
+    bigImage.style =`background: url("/assets/images/display_item${index}.jpg") no-repeat center center/cover;`
+    smallImages.forEach(smallImage => {
+        smallImage.classList.remove("image-active");
+    });
+    event.target.classList.add("image-active");
+}
+
+smallImages.forEach((smallImage, index) => {
+    smallImage.addEventListener('mouseenter',(event)=>onEnter(event,index))
+});
